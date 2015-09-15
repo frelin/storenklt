0f9e9435755ae050bc99961e0242642a817e313a
Fredrik Lindgren <noreply>
2015-09-15 06:29:03 +0000

    Add component list-view

---
+++ ui/list-view.reel/list-view.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/list-view.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class ListView
+ * @extends Component
+ */
+exports.ListView = Component.specialize(/** @lends ListView# */ {
+    constructor: {
+        value: function ListView() {
+            this.super();
+        }
+    }
+});
